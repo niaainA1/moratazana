dc5f44f9d555a675f271dc0e50dbd580022677d1
Dolphe <dolphe@Kali.lan>
2026-04-22 14:33:58 +0300

    Fix DemoApplication - conversion URL postgresql vers jdbc

--- banckend/src/main/java/com/example/demo/DemoApplication.java
+++ banckend/src/main/java/com/example/demo/DemoApplication.java
@@ -6,8 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        String databaseUrl = System.getenv("DATABASE_URL");
+        if (databaseUrl != null && databaseUrl.startsWith("postgresql://")) {
+            System.setProperty("spring.datasource.url",
+                databaseUrl.replace("postgresql://", "jdbc:postgresql://"));
+        }
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
